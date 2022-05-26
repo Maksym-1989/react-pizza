cd1c64d13339f9b8ee1495ea112029a94ea0b407
Maksym-1989 <zaksumy1989@gmail.com>
2022-05-26 15:02:26 +0300

    useState

--- src/components/pizzaList/PizzaList.jsx
+++ src/components/pizzaList/PizzaList.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import "./pizzaList.scss";
 
 const PizzaList = () => {
+  const [pizzaCount, setPizzaCount] = useState(0);
+
+  const addPizza = () => {
+    setPizzaCount(pizzaCount + 1);
+  };
+
   return (
     <ul className="pizza-block list">
       <li className="pizza-block__item ">
@@ -24,7 +31,7 @@ const PizzaList = () => {
         <div className="pizza-block__bottom">
           <span className="pizza-block__bottom-price">от 395 ₽</span>
 
-          <button className="button">
+          <button className="button" onClick={addPizza}>
             <svg
               className="button__svg"
               width="12"
@@ -35,7 +42,11 @@ const PizzaList = () => {
               <path d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z" />
             </svg>
             <p className="button__text">Добавить</p>
-            <span className="button__span">25</span>
+            {pizzaCount !== 0 ? (
+              <span className="button__span">{pizzaCount}</span>
+            ) : (
+              <></>
+            )}
           </button>
         </div>
       </li>
