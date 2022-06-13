cc542a2f8ed3d859af4b24ae41b91d6844ac2c0d
Maksym-1989 <zaksumy1989@gmail.com>
2022-06-13 17:43:31 +0300

    done

--- src/components/pizzaList/PizzaList.jsx
+++ src/components/pizzaList/PizzaList.jsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect } from "react";
 import qs from "qs";
 
 import PizzaListItem from "../pizzaListItem/PizzaListItem";
@@ -8,16 +7,20 @@ import Sceleton from "../sceleton/Sceleton";
 import styles from "./PizzaList.module.scss";
 
 import { list } from "../sort/Sort";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
+import { fetchPizzas } from "../../redux/slices/pizzasSlice";
 
 const PizzaList = () => {
-  const [pizzas, setPizzas] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const dispatch = useDispatch();
 
   const categoryIdx = useSelector((state) => state.filter.categoryIdx);
   const sortIdx = useSelector((state) => state.filter.sortIdx);
   const searchString = useSelector((state) => state.searchString);
+
+  const pizzas = useSelector((state) => state.pizzas.items);
+  const isLoading = useSelector((state) => state.pizzas.loading);
+  const error = useSelector((state) => state.pizzas.error);
 
   const filteredPizzas = pizzas.filter(({ name }) =>
     name.toLowerCase().includes(searchString.toLowerCase())
@@ -27,17 +30,7 @@ const PizzaList = () => {
   const params = qs.parse(useLocation().search.slice(1));
 
   useEffect(() => {
-    setIsLoading(true);
-    axios
-      .get(
-        `https://6294a42663b5d108c19025fe.mockapi.io/api/v1/items?category=${
-          categoryIdx !== 0 ? categoryIdx : ""
-        }&sortBy=${list[sortIdx].sortProperty}`
-      )
-      .then((res) => {
-        setPizzas(res.data);
-        setIsLoading(false);
-      });
+    dispatch(fetchPizzas({ categoryIdx, sortIdx }));
 
     const queryString = qs.stringify({
       category: categoryIdx,
@@ -45,14 +38,29 @@ const PizzaList = () => {
     });
 
     navigate(`?${queryString}`);
-  }, [categoryIdx, sortIdx, navigate]);
+  }, [categoryIdx, sortIdx, navigate, dispatch]);
 
   return (
-    <ul className={styles.pizza_block}>
-      {isLoading
-        ? [...new Array(9)].map((_, idx) => <Sceleton key={idx} />)
-        : filteredPizzas.map((obj) => <PizzaListItem {...obj} key={obj.id} />)}
-    </ul>
+    <>
+      {error ? (
+        <div className={styles.content__message}>
+          <h2>Произошла ошибка 😕</h2>
+          <p>
+            К сожалению, не удалось загрузить данные. Попробуйте повторить
+            попытку позже.
+          </p>
+          <p>{error.message}</p>
+        </div>
+      ) : (
+        <ul className={styles.pizza_block}>
+          {isLoading
+            ? [...new Array(9)].map((_, idx) => <Sceleton key={idx} />)
+            : filteredPizzas.map((obj) => (
+                <PizzaListItem {...obj} key={obj.id} />
+              ))}
+        </ul>
+      )}
+    </>
   );
 };
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import filterReducer from "./slices/filterSlice";
 import searchReducer from "./slices/searchSlice";
 import cartReducer from "./slices/cartSlice";
+import pizzasReducer from "./slices/pizzasSlice";
 
 export const store = configureStore({
   reducer: {
     filter: filterReducer,
     searchString: searchReducer,
     cart: cartReducer,
+    pizzas: pizzasReducer,
   },
 });
